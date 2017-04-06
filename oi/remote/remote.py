12e0fe2ca58da66b8bb90bf727e997cb94cb2ea0
jyy <jiangyy@outlook.com>
2017-04-06 14:25:14 +0800

    change upload

--- oi/remote/remote.py
+++ oi/remote/remote.py
@@ -1,54 +1,62 @@
 # -*- coding: utf-8 -*-
 import argparse, tempfile, shutil, os, subprocess
 from subprocess import PIPE
+from multiprocessing import Process
 
 def execute(args):
     p = subprocess.Popen(args, stdout=PIPE, stderr=PIPE)
     (out, err) = p.communicate()
     return (p.returncode, out, err)
 
-def remote_do(ip, *cmd):
+def remote_do(ip, cmd):
     SSH_OPTIONS = ["-i", key, "-o", "StrictHostKeyChecking=no", "-o", "ConnectTimeout={0}".format(timeout), "-p", port]
-    args = ["ssh"] + SSH_OPTIONS + [ip] + list(cmd)
+    args = ["ssh"] + SSH_OPTIONS + [ip] + cmd
     return execute(args)
 
-def remote_copy(ip, fname, dest):
+def remote_upload(ip, src, dest):
     SSH_OPTIONS = ["-i", key, "-o", "StrictHostKeyChecking=no", "-o", "ConnectTimeout={0}".format(timeout), "-P", port]
-    args = ["scp"] + SSH_OPTIONS + [fname, "{0}:{1}".format(ip, dest)]
+    args = ["scp"] + SSH_OPTIONS + [src, "{0}:{1}".format(ip, dest)]
+    return execute(args)
+
+def remote_download(ip, src, dest):
+    SSH_OPTIONS = ["-i", key, "-o", "StrictHostKeyChecking=no", "-o", "ConnectTimeout={0}".format(timeout), "-P", port]
+    args = ["scp"] + SSH_OPTIONS + ["{0}:{1}".format(ip, src)] + [dest]
     return execute(args)
 
 def oi_remote(args):
     global port, timeout, key
     if len(args) == 0: args = ['-h']
     parser = argparse.ArgumentParser(description = 'send a file to a destination')
-    parser.add_argument('-i', help = 'private key file')
-    parser.add_argument('-p', help = 'port', default = '22')
+    parser.add_argument('-key', help = 'private key file')
+    parser.add_argument('-port', help = 'port', default = '22')
     parser.add_argument('-timeout', help = 'time out (s)', default = '10')
-    parser.add_argument('files', help = 'files to be send', nargs = argparse.REMAINDER)
+    parser.add_argument('-host', help = 'host IP/name')
+    parser.add_argument('command', help = 'the command to be executed', choices = {"execute", "upload", "download"}, nargs = 1)
+    parser.add_argument('args', help = 'arguments', nargs = argparse.REMAINDER)
     options = vars(parser.parse_args(args))
 
-    key = options.get('i')
-    port = options.get('p')
+    key = options.get('key')
+    port = options.get('port')
     timeout = options.get('timeout')
-    args = options.get('files')
+    host = options.get('host')
+    cmd = options.get('command')[0]
+    args = options.get('args')
 
-    (ret, out, err) = remote_do('jsoi-admin@localhost', 'ls -l /media/sf_shared')
-    if ret == 0:
-        print out
-    else:
-        print "ERROR"
-        print err
+    # execute command
+    # upload src(host) dest(remote)
+    # download src(remote) dest(host)
 
-# TODO:
-# commands:
-#   send DIR -> return DIRNAME
-#   execute ls -l /media/sf_shared
-
-
-# Scenarios:
-#   1. upload a README.pdf
-#   2. upload a problem.pdf
-#   3. get files in the dir.
+    if cmd == 'execute':
+        (ret, out, err) = remote_do('jsoi-admin@{0}'.format(host), args)
+        if ret == 0: print out
+        exit(ret)
+    elif cmd == 'upload':
+        (ret, out, err) = remote_upload('jsoi-admin@{0}'.format(host), args[0], args[1])
+        if ret == 0: print out
+        exit(ret)
+    elif cmd == 'download':
+        (ret, out, err) = remote_download('jsoi-admin@{0}'.format(host), args[0], args[1])
+        if ret == 0: print out
 
 def oi_send_gui():
     pass
